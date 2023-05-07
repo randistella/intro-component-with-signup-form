2659d34a5c034154f615a43ec1482f4ac9fb9613
Randi Stella <RANDINICOLESTELLA@GMAIL.COM>
2023-05-07 15:28:43 -0500

    done

--- dist/app.js
+++ dist/app.js
@@ -3,17 +3,69 @@ const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const errorElement = document.getElementById('error');
 
-//stops page from submitting when there are errors
 form.addEventListener('submit', (e) => {
-  let messages = [];
-  if (fname.value === '' || fname.value == null) {
-    messages.push('Name is required');
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  // get the values from the inputs
+  const fnameValue = fname.value.trim();
+  const lnameValue = lname.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (fnameValue === '') {
+    // show error
+    // add error class
+    setErrorFor(fname, 'First Name cannot be empty');
+  } else {
+    // add success class
+    setSuccessFor(fname);
   }
 
-  if (messages.length > 0) {
-    e.preventDefault();
-    errorElement.innerText = messages.join(', ');
+  if (lnameValue === '') {
+    setErrorFor(lname, 'Last Name cannot be empty');
+  } else {
+    setSuccessFor(lname);
   }
-});
+
+  if (emailValue === '') {
+    setErrorFor(email, 'Email cannot be blank');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Not a valid email');
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === '') {
+    setErrorFor(password, 'Password cannot be empty');
+  } else {
+    setSuccessFor(password);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; //form-control
+  const small = formControl.querySelector('small');
+
+  //add error message inside of small
+  small.innerText = message;
+
+  //add error class
+  formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+//this is wrong
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
